b17f20add97468c8f1730ccbc73002508c1a4129
Unreal5trength <stevensalib2005@gmail.com>
2020-07-05 18:46:55 -0400

    Update platcraft.js

--- platcraft.js
+++ platcraft.js
@@ -21,7 +21,7 @@ const embed3 = new Discord.MessageEmbed()
     
     client.on('message', message => {
     
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    if (!message.content.startsWith(process.env.prefix) || message.author.bot) return;
     let args = message.content.substring(prefix.length).split(" ");
     const guild = message.guild
     switch(args[0]) {
