b2eace97b24c9d39dbefbf7be8ee91872987c7ce
Unreal5trength <stevensalib2005@gmail.com>
2020-07-05 18:22:55 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js')
 const client = new Discord.Client(); 
+const prefix = "-"
 let counter = 0;
 const embed1 = new Discord.MessageEmbed()
 .setTitle('**All Discord Commands:**')
