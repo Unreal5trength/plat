a6b198aab4e60621e37168241de093ce922dda65
Unreal5trength <stevensalib2005@gmail.com>
2020-07-20 23:46:45 -0400

    Update platcraft.js

--- platcraft.js
+++ platcraft.js
@@ -325,7 +325,7 @@ const embed3 = new Discord.MessageEmbed()
 client.on('guildMemberAdd', member => { 
     
     
-    member.guild.channels.find('734941676669501446').send({embed: {
+    member.guild.channels.fetch('734941676669501446').send({embed: {
     color: 3447003,
     title: "**SERVER NAME** Welcome Bot!",
     url: "WEBSITE URL",
