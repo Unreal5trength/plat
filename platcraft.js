1eb90aae7349a5769f8bbeb9db872438dbae1b61
Unreal5trength <stevensalib2005@gmail.com>
2020-08-04 16:44:10 -0400

    Update platcraft.js

--- platcraft.js
+++ platcraft.js
@@ -316,12 +316,34 @@ const embed3 = new Discord.MessageEmbed()
                                       });
                             }
                             break;
-                            
 
-                
-            }
-        }
-)
+                            case 'kick':
+                                if (!message.member.roles.cache.some(role => role.name === "Senior Staff")) {
+                                    message.channel.send("You do not have access to this command! Please ask a Staff Member to give you your transcript!")
+                                    return false;
+                                }
+                                const user = message.mentions.users.first();
+                                if (user) {
+                                  const member = message.guild.member(user);
+                                  if (member) {
+                                    member
+                                      .kick('Optional reason that will display in the audit logs')
+                                      .then(() => {
+                                        message.reply(`Successfully kicked ${user.tag}`);
+                                      })
+                                      .catch(err => {
+                                        message.reply('I was unable to kick the member');
+                                        console.error(err);
+                                      });
+                                  } else {
+                                    message.reply("That user isn't in this guild!");
+                                  }
+                                } else {
+                                  message.reply("You didn't mention the user to kick!");
+                                    }
+                                }
+                            }
+                        )
 
 client.on('message', message => {
     if (message.content.toLowerCase() === '-trans') {
